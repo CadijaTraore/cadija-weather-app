b8b7e9ad16ca6a6d510381771bcdd08cb0aa101d
CadijaTraore <98784824+CadijaTraore@users.noreply.github.com>
2022-06-18 13:55:01 +0100

    icon removed

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response){
-    console.log(response.data)
+console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -27,17 +27,26 @@ let descriptionElement = document.querySelector("#description");
 descriptionElement.innerHTML = (response.data.weather[0].description);
 
 let humidityElement = document.querySelector("#humidity");
-humidityElement.innerHTML = (response.data.main.humidity);
+humidityElement.innerHTML = (response.data.main.humidity); 
+// here, we're saying we want the ID element to get the atributes of the API data we ...
+//integrated in the code; to see the path we console log and check the object tree
 
 let windElement = document.querySelector("#wind");
 windElement.innerHTML = Math.round(response.data.wind.speed);
 
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML =  formatDate(response.data.dt *1000);
+
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+ // changing the city name accompanied by the right weather icon, we replace the id 02d to the object location//
+
+ iconElement.setAttribute("alt", response.data.weather[0].description); // this allows the alternative text image src to change according to the city name when inspectig the element section (not the console)
 }
 
 let apiKey = "11fcbbbce878c670c51e166618d82bd1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+let city = "Moscow";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature); 
