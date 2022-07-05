322b11a7c12088edb34af637044ee4be9d817670
CadijaTraore <98784824+CadijaTraore@users.noreply.github.com>
2022-07-05 15:50:32 +0100

    HTML removed from HTML fine

--- src/app.js
+++ src/app.js
@@ -15,6 +15,40 @@ function formatDate(timestamp) {
     return `${day} at ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    //repeating the forecast without using multiple blocks of heavy code 
+  
+    let forecastHTML = `<div class="row">`;
+   //creating a loop for the forecast
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day) { 
+
+   // concatenating the strings
+   forecastHTML = forecastHTML +  
+   `
+     <div class="row">
+            <div class="col-2"> 
+                <div class="weather-forecast-date"> 
+                ${day} 
+                <img src="https://openweathermap.org/img/wn/01d@2x.png" 
+                alt="" 
+                width="42">
+            </br>
+            <div class="weather-forecast-temperatures"> 
+                <span class="weather-forecast-temperature-max"> 
+                    18° </span>
+                <span class="weather-forecast-temperature-min"> 12° </span>
+            </div>
+            </div> 
+        </div>
+        </div>
+        `;
+     })    
+    forecastElement.innerHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response){
 
 let temperatureElement = document.querySelector("#temperature");
@@ -90,3 +124,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Lisbon");
+displayForecast();
